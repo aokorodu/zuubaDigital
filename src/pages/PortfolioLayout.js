41f7ff7c9aa919ed55803abd2a411c251c4337bf
aokorodu <aokorodu@gmail.com>
2023-09-10 16:04:34 -0400

    styling

--- src/pages/PortfolioLayout.js
+++ src/pages/PortfolioLayout.js
@@ -1,23 +1,26 @@
 import { Link, Outlet } from "react-router-dom";
+import styles from "./PortfolioLayout.module.css";
 
 export function PortfolioLayout() {
   return (
     <>
-      <div>
-        <div>
-          <Link to="/portfolio/games">games</Link>
+      <div className={styles.holder}>
+        <div className={styles.subnavHolder}>
+          <div>
+            <Link to="/portfolio/games">games</Link>
+          </div>
+          <div>
+            <Link to="/portfolio/experiments">experiments</Link>
+          </div>
+          <div>
+            <Link to="/portfolio/intSVGs">interactive SVGs</Link>
+          </div>
+          <div>
+            <Link to="/portfolio/prototypes">prototypes</Link>
+          </div>
         </div>
-        <div>
-          <Link to="/portfolio/experiments">experiments</Link>
-        </div>
-        <div>
-          <Link to="/portfolio/intSVGs">interactive SVGs</Link>
-        </div>
-        <div>
-          <Link to="/portfolio/prototypes">prototypes</Link>
-        </div>
+        <Outlet />
       </div>
-      <Outlet />
     </>
   );
 }

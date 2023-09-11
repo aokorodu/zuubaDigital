e41456b83b38db63ac2290ff911fea4c78e4f095
aokorodu <aokorodu@gmail.com>
2023-09-10 20:53:30 -0400

    more stuff

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -11,33 +11,24 @@ const navigation = [
 export function MainNav() {
   return (
     <div className={styles.navHolder}>
-      <nav className={styles.nav}>
-        {navigation.map((navitem) => {
-          return (
-            <NavLink
-              key={navitem.name}
-              to={navitem.link}
-              className={({ isActive }) => {
-                return isActive ? styles.active : styles.inactive;
-              }}
-            >
-              {navitem.name}
-            </NavLink>
-          );
-        })}
-        {/* <div>
-          <NavLink to="/">home</NavLink>
-        </div>
-        <div>
-          <NavLink to="/portfolio">portfolio</NavLink>
-        </div>
-        <div>
-          <NavLink to="/about">about</NavLink>
-        </div>
-        <div>
-          <NavLink to="/contact">contact</NavLink>
-        </div> */}
-      </nav>
+      <div className={styles.closeButton}>X</div>
+      <div className={styles.linkHolder}>
+        <nav className={styles.nav}>
+          {navigation.map((navitem) => {
+            return (
+              <NavLink
+                key={navitem.name}
+                to={navitem.link}
+                className={({ isActive }) => {
+                  return isActive ? styles.active : styles.inactive;
+                }}
+              >
+                {navitem.name}
+              </NavLink>
+            );
+          })}
+        </nav>
+      </div>
     </div>
   );
 }

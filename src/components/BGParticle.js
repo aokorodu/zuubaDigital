a9660db3b61084a552f8c966349166e150fc3245
aokorodu <aokorodu@gmail.com>
2023-09-20 19:44:53 -0400

    particle tweaks

--- src/components/BGParticle.js
+++ src/components/BGParticle.js
@@ -5,14 +5,15 @@ class BGParticle extends React.Component {
     super();
     this.index = index;
     this.x = Math.random() * 1000;
-    this.speed = 0.2;
-    this.r = 10 + Math.round(Math.random() * 30);
+    this.speed = 0.5;
+    this.minRadius = 20;
+    this.r = 3 + Math.round(Math.random() * this.minRadius);
     this.radians = ((Math.PI * 2) / 1000) * this.x;
-    this.amplitude = 10 + Math.round(Math.random() * 200);
+    this.amplitude = Math.round(Math.random() * 200);
     this.y = 0 + Math.cos(this.radians) * this.amplitude;
     this.position = { x: this.x, y: this.y };
-    this.hue = 180 + Math.round(Math.random() * 10);
-    this.color = `hsl(${this.hue}, 100%, 70%)`;
+    this.hue = Math.round(Math.random() * 360);
+    this.color = `hsl(${this.hue}, 100%, 90%)`;
     this.holder = null;
     this.isSin = Math.random() > 0.5 ? true : false;
   }
@@ -21,7 +22,7 @@ class BGParticle extends React.Component {
     if (this.holder === null) {
       this.holder = document.querySelector(`#circ_${this.index}`);
     }
-    this.position.x += 0.2;
+    this.position.x += 0.5;
     if (this.position.x > 1000 + this.r) this.position.x = -this.r;
     this.radians = ((Math.PI * 2) / 1000) * this.position.x;
     this.position.y = this.isSin
@@ -39,8 +40,14 @@ class BGParticle extends React.Component {
         id={`circ_${this.index}`}
         transform={`translate(${this.position.x} ${this.position.y})`}
       >
-        <circle cx={0} cy={0} r={this.r} fill={this.color} opacity={0.05} />
-        <circle cx={0} cy={0} r={this.r / 2} fill={this.color} opacity={0.05} />
+        <circle
+          cx={0}
+          cy={0}
+          r={this.r}
+          fill={this.color}
+          fill-opacity={0.15}
+        />
+        {/* <circle cx={0} cy={0} r={this.r / 2} fill={this.color} opacity={0.05} /> */}
       </g>
     );
   }

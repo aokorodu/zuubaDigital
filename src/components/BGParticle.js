ef9c3992e2b523a8241df89c45f370454cf6462a
aokorodu <aokorodu@gmail.com>
2023-09-23 15:44:20 -0400

    more tweaks and refactoring

--- src/components/BGParticle.js
+++ src/components/BGParticle.js
@@ -72,12 +72,30 @@ class BGParticle extends React.Component {
           : this.midline + Math.cos(this.radians) * this.amplitude;
         return pos;
       },
+      target: new PVector(0, 0),
+      setTarget(newTarget) {
+        this.target = newTarget;
+      },
+      inSinPosition: function (currentPos) {
+        const dist = PVector.GetDistance(currentPos, this.target);
+        const isClose = dist < 1 ? true : false;
+
+        return isClose;
+      },
+      moveToSinPosition: function (currentPos) {
+        const dx = this.target.x - currentPos.x;
+        const moveX = dx / 10;
+        currentPos.x += moveX;
+
+        const dy = this.target.y - currentPos.y;
+        const moveY = dy / 10;
+        currentPos.y += moveY;
+      },
     };
 
     // sin vars
-
     this.trig.setRadians(Math.PI * 2 * (this.physics.position.x / 1000));
-    this.target = new PVector(this.physics.position.x, this.trig.ypos);
+    this.trig.setTarget(new PVector(this.physics.position.x, this.trig.ypos));
 
     // color vars
     this.hue = Math.round(Math.random() * 360);
@@ -114,33 +132,22 @@ class BGParticle extends React.Component {
 
   // sinFlow ------------
 
-  inSinPosition() {
-    const dist = PVector.GetDistance(this.physics.position, this.target);
-    const isClose = dist < 1 ? true : false;
-
-    return isClose;
-  }
-
   moveToSinPosition() {
-    const dx = this.target.x - this.physics.position.x;
+    const dx = this.trig.target.x - this.physics.position.x;
     const moveX = dx / 10;
     this.physics.position.x += moveX;
 
-    const dy = this.target.y - this.physics.position.y;
+    const dy = this.trig.target.y - this.physics.position.y;
     const moveY = dy / 10;
     this.physics.position.y += moveY;
   }
 
   sinFlow() {
-    console.log(
-      "this.trig.readyToStartSinFlow? ",
-      this.trig.readyToStartSinFlow
-    );
     if (!this.trig.readyToStartSinFlow) {
-      if (this.inSinPosition()) {
+      if (this.trig.inSinPosition(this.physics.position)) {
         this.trig.readyToStartSinFlow = true;
       } else {
-        this.moveToSinPosition();
+        this.trig.moveToSinPosition(this.physics.position);
         return;
       }
     }

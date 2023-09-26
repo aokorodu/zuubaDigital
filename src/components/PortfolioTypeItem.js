5b77b848661c5d78bddb4cfb772e5268cb596a50
aokorodu <aokorodu@gmail.com>
2023-09-26 17:59:09 -0400

    fixing up about pg styling

--- src/components/PortfolioTypeItem.js
+++ src/components/PortfolioTypeItem.js
@@ -10,43 +10,43 @@ export function PortfolioTypeItem({
   pauseOverlay,
 }) {
   return (
-    <div className={styles.card}>
-      <NavLink key={`ptitem_${name}`} to={link}>
-        <div className={styles.imageHolder}>
-          {imagelink && (
-            <img src={imagelink} style={{ width: "100%", height: "200px" }} />
-          )}
-          {videoLink && (
-            // <video width="100%" height="100%">
-            //   <source src={videoLink} type="video/mp4" />
-            //   Your browser does not support the video tag.
-            // </video>
-            <HoverVideoPlayer
-              videoSrc={videoLink}
-              sizingMode="overlay"
-              restartOnPaused
-              pausedOverlay={
-                <img
-                  src={pauseOverlay}
-                  alt=""
-                  style={{
-                    // Make the image expand to cover the video's dimensions
-                    width: "200px",
-                    height: "200px",
-                    objectFit: "cover",
-                  }}
+    <div>
+      <div className={styles.card}>
+        <NavLink key={`ptitem_${name}`} to={link}>
+          <div className={styles.imageHolder}>
+            {imagelink && (
+              <img src={imagelink} style={{ width: "100%", height: "200px" }} />
+            )}
+            {videoLink && (
+              <>
+                <HoverVideoPlayer
+                  videoSrc={videoLink}
+                  sizingMode="overlay"
+                  restartOnPaused
+                  pausedOverlay={
+                    <img
+                      src={pauseOverlay}
+                      alt=""
+                      style={{
+                        width: "200px",
+                        height: "200px",
+                        objectFit: "cover",
+                      }}
+                    />
+                  }
+
+                  //  loadingOverlay={
+                  //    <div className="loading-overlay">
+                  //      <div className="loading-spinner" />
+                  //    </div>
+                  //  }
                 />
-              }
-              //  loadingOverlay={
-              //    <div className="loading-overlay">
-              //      <div className="loading-spinner" />
-              //    </div>
-              //  }
-            />
-          )}
-        </div>
-        <div className={styles.textHolder}>{name}</div>
-      </NavLink>
+              </>
+            )}
+          </div>
+        </NavLink>
+      </div>
+      <div className={styles.textHolder}>{name}</div>
     </div>
   );
 }

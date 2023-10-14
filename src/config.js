ca7ccc01aa4034e3339492734e4fdc5e83e73a90
aokorodu <aokorodu@gmail.com>
2023-10-13 23:50:42 -0400

    added prototypes data to config

--- src/config.js
+++ src/config.js
@@ -31,4 +31,31 @@ export const navConfig = {
     { name: "", imagelink: "./gen_art/art_fractal_trees_pink.jpeg" },
     { name: "", imagelink: "./gen_art/art_fractal_trees.png" },
   ],
+  prototypes: [
+    {
+      title: "Xfinity dashboard app platform - (video)",
+      description:
+        "High-fidelity prototype of Comcast’s “Dashboard” app platform, demonstrated live during Comcast’s Brian Roberts 2013 NCTA (National Cable & Telecommunications Association) keynote address. (The dashboard demo starts at 16:30 and runs through 17:50)",
+      links: [{ name: "link", link: "https://youtu.be/1CRjDc_yiBI?t=16m30s" }],
+    },
+    {
+      title: "401k Visualizer",
+      description:
+        "Interactive SVG demonstrating a 401k visualizer concept (desktop only)",
+      tech: "JavaScript, GSAP, SVG.",
+      links: [
+        {
+          name: "link",
+          link: "https://aokorodu.github.io/bench_mo_money/index.html",
+        },
+      ],
+    },
+    {
+      title: "401k Visualizer - Abstract",
+      description:
+        "A more abstract, experimental version of the Interactive 401k visualizer with particle effects, built using canvas/PixiJS (desktop only)",
+      tech: "PixiJS, GSAP, Canvas.",
+      links: [{ name: "link", link: "https://aokorodu.github.io/portfolio/" }],
+    },
+  ],
 };

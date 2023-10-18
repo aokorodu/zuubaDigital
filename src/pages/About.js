89aff9d1e124ee12495edacd6f758f158d0a860e
aokorodu <aokorodu@gmail.com>
2023-10-17 22:09:52 -0400

    new about text

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,15 +20,15 @@ export function About() {
             {!loaded && <Spinner />}
           </div>
 
+          {/* <div className={styles.description}>
+            zuubaDigital is Abraham Okorodudu. I am an interactive developer
+            with over 15 years of experience developing interactive animations,
+            animated infographics, high-fidelity prototypes, websites, games,
+            and applications.
+          </div> */}
           <div className={styles.description}>
-            I am an interactive developer with over 15 years of experience
-            developing interactive animations, animated infographics,
-            high-fidelity prototypes, websites, games, and applications. The
-            types of environments in which I have worked are quite varied - from
-            large, fast-paced interactive ad agencies to medium-sized technology
-            companies and small start-ups. I have had the ability at each stop
-            along the way to quickly adapt to changing and evolving technologies
-            and to learn new skills.
+            Creative coder, digital artist, former semi-pro basketball player,
+            world traveler, father, husband, brother, son.
           </div>
         </div>
       </div>
